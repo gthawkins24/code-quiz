e31b916b79a28e3c39899655ba932d36ad6318bf
Gregory Hawkins <91228189+gthawkins24@users.noreply.github.com>
2021-12-30 23:05:55 -0500

    initial commit, started html/css structure

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const highScoresButton = document.querySelector('#highScores');
+
+const test = function() {
+    alert('You clicked me!');
+}
+highScoresButton.addEventListener('click', test);
