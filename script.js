114ab384ad727227010d62ba15e4b9a13c768eee
Gregory Hawkins <91228189+gthawkins24@users.noreply.github.com>
2021-12-31 01:24:04 -0500

    beginning html structure finished, started JS

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 'use strict';
 
 const highScoresButton = document.querySelector('#highScores');
+const startButton = document.querySelector('#startButton');
+const introBox = document.querySelector('#introBox');
+const questionBox = document.querySelector('#question');
+const answersBox = document.querySelector('#answers');
 
-const test = function() {
+highScoresButton.addEventListener('click', function() {
     alert('You clicked me!');
-}
-highScoresButton.addEventListener('click', test);
+});
+
+startButton.addEventListener('click', function() {
+    introBox.style.display = "none";
+    questionBox.style.display = "block";
+});
